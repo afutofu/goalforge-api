caf3ff83307cd885417deb24471afd57b8803270
Muhammad Afuzarahman <theafuza@gmail.com>
2024-03-30 23:56:19 -0700

    Implement naive register user

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 import boto3
+import os
+from dotenv import load_dotenv
 
-from flask import current_app
+load_dotenv()
 
 dynamodb_client = boto3.resource(
     "dynamodb",
-    aws_access_key_id=current_app.config["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key=current_app.config["AWS_SECRET_ACCESS_KEY"],
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_REGION"),
 )
--- routes/auth.py
+++ routes/auth.py
@@ -1,32 +1,63 @@
 from flask import Blueprint, current_app, request, jsonify
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 import jwt
+from database import dynamodb_client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 auth_blueprint: Blueprint = Blueprint("auth", __name__)
 
 
-@auth_blueprint.route("/api/generate-jwt", methods=["POST"])
+@auth_blueprint.route("/generate-jwt", methods=["POST"])
 def generate_jwt():
     # Assuming you are sending JSON data
-    user_data = request.json
-    email = user_data.get("email")
-    name = user_data.get("name")
+    sign_in_data = request.json
+    sign_in_type = sign_in_data.get("sign_in_type")
+    email = sign_in_data.get("email")
+    name = sign_in_data.get("name")
+
+    try:
+        users_table = dynamodb_client.Table("GoalForge-Users")
+    except Exception as e:
+        print("Error: ", e)
+        return jsonify({"error": "Internal server error"}), 500
+
+    print("test generate_jwt")
+
+    # Check if data is valid
+    if not sign_in_type or not email or not name:
+        response = {"error": "Invalid data"}
+        return jsonify(response), 400
 
     # Check if user exists in the databse
+    user = None
+    if sign_in_type == "google":
+        user = users_table.get_item(Key={"UserID": email})
 
     # If user does not exist, register the user in the database
+    if not "Item" in user or not user["Item"]:
+        users_table.put_item(
+            Item={
+                "UserID": email,
+                "Name": name,
+                "SignInType": sign_in_type,
+                "CreatedAt": str(datetime.now(tz=timezone.utc)),
+            }
+        )
 
     # In either case, generate a JWT for the user containing the unique user ID
 
     # Generate a JWT
     encoded_jwt = jwt.encode(
         {
-            "email": email,
+            "userID": email,
             "name": name,
-            "exp": datetime.datetime.utcnow()
-            + datetime.timedelta(hours=24),  # 24-hour expiration
+            "exp": datetime.now(tz=timezone.utc)
+            + timedelta(hours=24),  # 24-hour expiration
         },
-        current_app.config["JWT_SECRET_KEY"],
+        os.getenv("JWT_SECRET_KEY"),
         algorithm="HS256",
     )
 
