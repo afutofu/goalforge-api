40923e00a63fb38c9199ee1a89e3b8006aef797e
Muhammad Afuzarahman <theafuza@gmail.com>
2024-05-06 01:39:19 -0700

    Disable OAuthlib HTTPs verification when in dev

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from routes.tasks import tasks_blueprint
 from routes.activity_logs import activity_logs_blueprint
 from routes.auth import auth_blueprint
+import os
 
 app = Flask(__name__)
 
@@ -14,5 +15,10 @@ app.register_blueprint(auth_blueprint, url_prefix="/api/v1/auth")
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0", ssl_context=ctx)
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
     app.run()
     # app.run(debug=True, port=8080)
